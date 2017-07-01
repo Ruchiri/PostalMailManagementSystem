4667b5d2d4d18bd2111dbe488f2b4cd86b49f5d9
Supimi Piumika <supimigamage@gmail.com>
2017-07-01 12:29:48 +0530

    reply letter formated & search modified

---
+++ replyLetter.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Reply Letter</title>
+    <link rel="stylesheet" href="css/replyletter.css">
+</head>
+<body>
+<div class="wrapper">
+    <div class="top-bar">
+        <h1>පිළිතුරු ලිපිය</h1>
+    </div>
+    <div class="reply-letter-model">
+        <ul>
+            <li>
+                <strong>යවන්නාගේ ලිපිනය</strong><br/>
+                <textarea name="txtsenderAddress" cols="80" rows="8"></textarea>
+
+            </li>
+
+            <li>
+                <strong>දිනය</strong><br/>
+                <input type="date" name="ddate">
+            </li>
+
+            <li>
+                <strong>ලබන්නාගේ ලිපිනය</strong><br/>
+                <textarea name="txtreceiverAddress" cols="80" rows="8"></textarea>
+            </li>
+
+            <li>
+                <strong>අන්තර්ගතය</strong><br/>
+                <textarea name="txtletterBody" cols="130" rows="40"></textarea>
+            </li>
+
+            <li>
+                <strong>අවසානය</strong><br/>
+                <textarea name="txtend" cols="50" rows="5"></textarea>
+            </li>
+        </ul>
+
+    </div>
+</div>
+
+
+</body>
+</html>
