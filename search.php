c8119e22afffea7b51d02d0a9367e2217d84a53a
Supimi Piumika <supimigamage@gmail.com>
2017-07-01 12:49:06 +0530

    completely changed the search window

--- search.php
+++ search.php
@@ -4,66 +4,85 @@
     <meta charset="UTF-8">
     <title>Search</title>
     <link rel="stylesheet" href="css/search.css">
-
 </head>
 <body>
 <div class="wrapper">
-<div class="top-bar-search">
-    <h1>තැපැල්පත් සෙවීම</h1>
+    <div class="top-bar-search">
+        <h1>තැපැල්පත් සෙවීම</h1>
+    </div>
+</div>
+<div class="seach-Model">
+    <div class="search-bar">
+        <div class="criteria1">
+            <h3>තැපැල්පත් අංකය</h3>
+        </div>
+
+        <div class="criteria2">
+            <h3>දිනය</h3>
+        </div>
+
+
+        <div class="criteria3">
+            <h3>අංශය</h3>
+        </div>
+
+
+        <div class="criteria4">
+            <h3>විෂය</h3>
+        </div>
+
+        <div class="criteria5">
+            <h3>ලිපිය එවූ පාර්ශවය</h3>
+        </div>
+    </div>
+
+
+    <div class="search-bottom-bar">
+        <div class="txtLetterNo">
+            <p><input type="text" name="Letter number"/></p>
+        </div>
+        <div class="dDate">
+            <p><input type="date"/></p>
+        </div>
+        <div class="txtSection">
+            <P>
+                <input type="text" list="sections">
+                <datalist id="sections">
+                    <option> ප්‍රධාන පරිශීලක</option>
+                    <option> ආයතන</option>
+                    <option> ගිනුම් අංශය</option>
+                    <option> සංවර්ධන අංශය</option>
+                    <option> ඉඩම් අංශය</option>
+                    <option> සමාජ සේවා අංශය</option>
+                    <option> දිවි නැගුම අංශය</option>
+                    <option> ක්ෂේත්‍ර</option>
+                    <option> ලියාපදිංචි අංශය</option>
+                    <option> මුදල් හා චෙක්පත් අංශය</option>
+                    <option> ප්‍රධාන නිලධාරී</option>
+                </datalist>
+            </P>
+
+        </div>
+        <div class="txtSubject">
+            <p><input type="text" name="Subject"></p>
+        </div>
+        <div class="lstSender">
+            <p><input type="text" name="Sender"></p>
+        </div>
+    </div>
+    <br>
+
+    <div class="search-button">
+        <button type="button">සොයන්න</button>
+    </div>
+    <br>
+
+    <div class="search-results">
+        <p>searching results...............................</p>
+    </div>
+
 
 </div>
 
-<table>
-    <tr class="headings">
-        <th>තැපැල්පත් අංකය</th>
-        <th>දිනය</th>
-        <th>අංශය</th>
-        <th>විෂය</th>
-        <th>ලිපිය එවූ පාර්ශවය</th>
-    </tr>
-
-    <tr>
-    <th><input type="text" name="Letter number"/></th>
-    <th><input type="date"/></th>
-    <th>
-        <ul>
-            <input type="text" list="sections">
-            <datalist id="sections">
-                <option> ප්‍රධාන පරිශීලක</option>
-                <option> ආයතන</option>
-                <option> ගිනුම් අංශය</option>
-                <option> සංවර්ධන අංශය</option>
-                <option> ඉඩම් අංශය</option>
-                <option> සමාජ සේවා අංශය</option>
-                <option> දිවි නැගුම අංශය</option>
-                <option> ක්ෂේත්‍ර</option>
-                <option> ලියාපදිංචි අංශය</option>
-                <option> මුදල් හා චෙක්පත් අංශය</option>
-                <option> ප්‍රධාන නිලධාරී</option>
-            </datalist>
-        </ul>
-
-    </th>
-    <th><input type="text" name="Subject"></th>
-    <th><input type="text" name="Sender"></th>
-
-    </tr>
-
-    <tr>
-        <td>
-            <div class="search-button">
-                <button type="button" >සොයන්න</button>
-            </div>
-
-        </td>
-    </tr>
-
-</table>
-
-<div class="search-results">
-    <p>searching results...............................</p>
-</div>
-
-</div>
 </body>
 </html>
