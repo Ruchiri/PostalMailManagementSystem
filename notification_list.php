fc9b45b62da7f066e146b94b9d891bd4f799bcd0
Ruchiri <yasararuchiri.15@cse.mrt.ac.lk>
2017-07-14 08:54:36 +0530

    notification list

--- notification_list.php
+++ notification_list.php
@@ -1,5 +1,6 @@
 <?php
 include "connect.php";
+$connection=connect();
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- report_section.php
+++ report_section.php
@@ -1,5 +1,6 @@
 <?php
-include ("connect.php");
+include "connect.php";
+$connection=connect();
 ?>
 <!DOCTYPE html>
 <head>
