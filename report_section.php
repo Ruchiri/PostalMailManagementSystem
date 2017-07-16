36548eaa53cb6766d7e0551e5db79f2a4959e6c6
Ruchiri <yasararuchiri.15@cse.mrt.ac.lk>
2017-07-16 10:37:07 +0530

    report section-validation added

--- report_section.php
+++ report_section.php
@@ -7,8 +7,20 @@ $section = $_SESSION['section'];
 <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF8">
     <meta charset="UTF-8">
+    <A HREF="javascript:history.go()"></A>
     <title>වාර්තා ලබා ගැනීම</title>
     <link rel="stylesheet" href="css/report_section.css">
+    <script type="text/javascript">
+        function checkForm(form) {
+            if(form.date1.value=="" || form.date2.value==""){
+                alert("Enter dates");
+                return false;
+            }else{
+                return true;
+            }
+        }
+
+    </script>
 </head>
 <body>
 <div class="Background">
@@ -19,30 +31,30 @@ $section = $_SESSION['section'];
         <p><strong>වාර්තා ලබා ගැනීම</strong></p>
     </div><!--Heading-->
     <div class="Choose-date">
-        <form action="report.php" method="get">
+        <form name="myForm" action="report.php" method="get" onsubmit="return checkForm(this)">
          <ul>
             <p><strong>අදාල කාල පරාසය තෝරන්න</strong></p>
             <div class=" Start-date">
                 <ul>
                     <p>ආරම්භක සෙවුම් දිනය</p>
-                    <input type="date" name="date1" id="date1" oninvalid="this.setCustomValidity('')" required/>
+                    <input type="date" name="date1" id="date1" />
                 </ul>
             </div><!--start-date-->
             <div class="End-date">
                 <ul>
                     <p>අවසාන සෙවුම් දිනය</p>
-                    <input type="date" name="date2" id="date2" required/>
+                    <input type="date" name="date2" id="date2" />
                 </ul>
             </div><!--End-date-->
 
              <div class="generate">
-                 <input type="submit" name="report"  value="වාර්තා ලබා ගැනීම" >
+                 <input type="submit" name="report"  value="වාර්තා ලබා ගැනීම"   >
+
              </div>
-
-
              </a>
         </ul>
         </form>
+
     </div><!--Choose-date-->
 
 </div><!--Background-->
